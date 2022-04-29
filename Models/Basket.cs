a00f538dce33454413f827359dd2ba37cac45301
Колосс Матвій <kolossmatveyka@gmail.com>
2022-04-29 23:38:25 +0300

    model for basket

---
+++ Models/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SanTech.Models
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+    }
+}
